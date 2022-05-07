8e12b12ffa1641d207e0c30ea89b573a777ee897
Qasem Talaee <qasem.talaee1375@gmail.com>
2022-05-07 21:01:51 +0430

    fix bug in wine.sh

--- bash/wine.sh
+++ bash/wine.sh
@@ -6,30 +6,24 @@
 
 # Updating repos info using apt
 sudo apt update
-if [ $? -eq 0 ]  ; then
+if [ $? -ne 0 ]  ; then
   #echo "[INFO] apt update is Done!"
   exit 0
-  else
-  exit 1
 fi
 
 # Installing recommended packages (apt-transport-https & ...)
 sudo apt install software-properties-common apt-transport-https wget -y
-if [ $? -eq 0 ]  ; then
+if [ $? -ne 0 ]  ; then
   #echo "[INFO] Installing recommended packages is Done!"
   exit 0
-  else
-  exit 1
 fi
 
 # Add i386 packages for wine, Required!
 # ToDo: Find a way to detect ARM systems
 sudo dpkg --add-architecture i386
-if [ $? -eq 0 ]  ; then
+if [ $? -ne 0 ]  ; then
   #echo "[INFO] Adding i386 repos is Done!"
   exit 0
-  else
-  exit 1
 fi
 
 # You have two way for downloading and installing wine:
@@ -47,22 +41,18 @@ fi
 
 # 1 (default):
 sudo apt install --install-recommends wine-stable -y
-if [ $? -eq 0 ]  ; then
+if [ $? -ne 0 ]  ; then
   #echo "[INFO] Installing wine is Done!"
   exit 0
-  else
-  exit 1
 fi
 
 # Installing Mesa vulkan driver & dxvk for wine
 # ToDo: Find a way to detect nouveau for not installing mesa-vulkan-drivers & dxvk (nouveau does not support Vulkan!)
 # ToDo: Find a way to detect NVIDIA Proprietary driver. its not using mesa-vulkan-driver!
 sudo apt install mesa-vulkan-drivers mesa-vulkan-drivers:i386 libvulkan1 libvulkan1:i386 *vulkan* *dxvk* -y
-if [ $? -eq 0 ]  ; then
+if [ $? -ne 0 ]  ; then
   #echo "[INFO] Installing vulkan driver & dxvk is Done!"
   exit 0
-  else
-  exit 1
 fi
 
 # Checking wine version
